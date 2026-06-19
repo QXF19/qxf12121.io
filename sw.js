17fa5a7799006792b312851835899e33fa1b5924
QXF19 <qxf1975@outlook.com>
2026-06-19 11:51:19 +0800

    Site updated: 2026-06-19 11:51:16

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1778773860385";const preCache = ["/images/taichi.png","/images/800101.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1781841064184";const preCache = ["/images/taichi.png","/images/800101.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
